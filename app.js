0dc3ef87537ab6f76e67fda6d09d82b972c69581
David <Rigby335@hotmail.com>
2022-05-03 15:35:18 -0700

    save button work

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ import { renderIngredients } from './utils.js';
 
 const form = document.getElementById('add-ingredient');
 const ingredientList = document.getElementById('ingredient-list');
+const mealList = document.getElementById('meal-list');
+const mealName = document.getElementById('meal-name')
 
 const remove = document.getElementById('remove');
 const save = document.getElementById('submit');
 // let state
 let ingredientArr = [];
+let mealArr = [];
 
 // set event listeners 
 function renderIngredient() {
@@ -17,15 +20,6 @@ function renderIngredient() {
         ingredientList.append(li);
     }
 }
-
-// function displayIngredient() {
-
-// }
-
-// function resetIngredients() {
-//     ingredientArr = [];
-//     ingredientList.textContent = '';
-// }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -43,10 +37,31 @@ form.addEventListener('submit', (e) => {
 
 });
 
+save.addEventListener('click', () => {
+    const name = mealName.value;
+    const total = ingredientArr.length;
+    mealArr.push({ name, total });
+    renderMeals();
+    resetIngredients();
+});
+
 remove.addEventListener('click', () => {
     ingredientArr.pop();
     renderIngredient();
 });
+
+function resetIngredients() {
+    ingredientArr = 0;
+    ingredientList.textContent = '';
+}
+
+function renderMeals() {
+    mealList.textContent = '';
+    for (let meal of mealArr) {
+        const li = renderMealLi(meal);
+        mealList.append(li);
+    }
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
